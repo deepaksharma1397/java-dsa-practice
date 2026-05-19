37fc0776409218c10d8cf2df44a9942d65fb6908
Deepak Sharma <ds5422173@gmail.com>
2026-05-19 16:14:31 +0530

    Added DSA folders and files

---
+++ 01- Basics/Basics - Problems/PrintEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PrintEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the limit: ");
+        int n = sc.nextInt();
+        System.out.println("Even numbers from 1 to " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ 01- Basics/Basics - Problems/SumOfNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How Many Numbers you want to Sum: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 1; i <= n; i++) {
+            int number = sc.nextInt();
+            sum += number;
+        }
+        System.out.println("The sum of " + n + " numbers is: " + sum);
+    }
+}
