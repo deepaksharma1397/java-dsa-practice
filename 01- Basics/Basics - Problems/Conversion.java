44366d6fd4094a57180866ed1eb7809f53da40e4
Deepak Sharma <deepak.sharma238@lpu.in>
2026-05-26 23:17:17 +0530

    Code Modified

--- 01- Basics/Basics - Problems/Conversion.java
+++ 01- Basics/Basics - Problems/Conversion.java
@@ -1,3 +1,12 @@
+// double to int 
+
+
+
 public class Conversion {
-    
+    public static void main (String[] args) {
+        double d = 3.14;
+        int i = (int) d; // This is called type casting, we are converting a double to an int
+        System.out.println("The value of d is: " + d);
+        System.out.println("The value of i is: " + i);
+    }
 }
