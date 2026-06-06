239aede52f4215b23dc4c65001bca9a5516d41fa
Deepak Sharma <deepak.sharma238@lpu.in>
2026-06-06 10:00:00 +0530

    Functions: add even or odd program

---
+++ 01- Basics/Functions/EvenorOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EvenorOdd {
+    static boolean isEven(int n){
+        return n % 2 == 0;
+    }
+
+
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (isEven(n)) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+        sc.close();
+                    }
+
+}
