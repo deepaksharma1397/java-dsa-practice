beed8e67a12bae4296b62a800a30931ff8ef7190
Deepak Sharma <ds5422173@gmail.com>
2026-09-18 01:51:25 +0530

    Add LeetCode 2235 solution

---
+++ 23-LeetCode-Daily/Add_Two_Integer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution{
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = add(a, b);
+        System.out.println(sum);
+    }
+}
